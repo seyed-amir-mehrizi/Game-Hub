c6409198553d7f4b2c3dc4a37855127535a67a17
seyed-amir-mehrizi <60378911+seyed-amir-mehrizi@users.noreply.github.com>
2023-06-19 11:47:29 +0330

    adding layout to the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 
 function App() {
 
   return (
-    <div>
-      <Button colorScheme='blue'>Button</Button>
-    </div>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold' >aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='blue'>main</GridItem>
+    </Grid>
   )
 }
 
